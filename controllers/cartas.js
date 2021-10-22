f14bc49b451492cf1ffa7e4ad388d0f2a9dd049a
ivandumas <A01375560@itesm.mx>
2021-10-22 02:45:32 -0500

    Me di cuenta de un error profe :(

--- controllers/cartas.js
+++ controllers/cartas.js
@@ -1,76 +1,77 @@
 const path = require('path')
 const Carta = require('../utils/database').models.carta
 
-exports.postAgregarCarta = (req, res)=>{
+exports.postAgregarCarta = (req, res) => {
     console.log(req.body)
-    const found = Carta.findOne({
-        where : { 
-            val : req.body.val,
-            palo : req.body.palo
+    Carta.findOne({
+        where: {
+            val: req.body.val,
+            palo: req.body.palo
         }
-    });
-    if (found === null) {
-        Carta.create(req.body)
-        .then(result=>{
-            console.log("Carta creada exitosamente")
-            res.json({estado:"aceptado"})
-        })
-        .catch((err)=>{
-            console.log(err)
-            res.json({estado:"error"})
-        })   
-    } else {
-        console.log("La Carta ya existe")
-        res.json({estado:"ya existe"})
-    }
+    }).then(found => {
+        if (found === null) {
+            Carta.create(req.body)
+                .then(result => {
+                    console.log("Carta creada exitosamente")
+                    res.json({ estado: "aceptado" })
+                })
+                .catch((err) => {
+                    console.log(err)
+                    res.json({ estado: "error" })
+                })
+        } else {
+            console.log("La Carta ya existe")
+            res.json({ estado: "ya existe" })
+        }
+    })
 }
 
 
-exports.getConsultarCartas = (req, res)=>{
+exports.getConsultarCartas = (req, res) => {
     Carta.findAll()
-        .then(carta=>{
+        .then(carta => {
             console.log(carta)
             res.json(carta)
         })
-        .catch((err)=>{
+        .catch((err) => {
             console.log(err)
-            res.json({estado:"error"})
+            res.json({ estado: "error" })
         })
 }
 
-exports.postBorrarCarta = (req, res)=>{
+exports.postBorrarCarta = (req, res) => {
     console.log(req.body)
     Carta.destroy({
-        where:{
-            id:req.body.id
+        where: {
+            id: req.body.id
         }
     })
-    .then(()=>{
-        console.log("Registro de carta eliminado")
-        res.json({estado:"aceptado"})
-    })
-    .catch((err)=>{
-        console.log(err)
-        res.json({estado:"error"})
-    })
+        .then(() => {
+            console.log("Registro de carta eliminado")
+            res.json({ estado: "aceptado" })
+        })
+        .catch((err) => {
+            console.log(err)
+            res.json({ estado: "error" })
+        })
 }
-exports.postActualizarCarta = (req, res)=>{
+exports.postActualizarCarta = (req, res) => {
     console.log(req.body)
-    
+
     Carta.update({
         val: req.body.val,
         palo: req.body.palo
-    },{
-        where:{
-            id:req.body.id
+    }, {
+        where: {
+            id: req.body.id
         }
     })
-    .then(()=>{
-        console.log("Registro de carta actualizado")
-        res.json({estado:"aceptado"})
-    })
-    .catch((err)=>{
-        console.log(err)
-        res.json({estado:"error"})
-    })
+        .then(() => {
+            console.log("Registro de carta actualizado")
+            res.json({ estado: "aceptado" })
+        })
+        .catch((err) => {
+            console.log(err)
+            res.json({ estado: "error" })
+        })
 }
